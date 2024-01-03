d550192a90e657a48b561dd0db881d2024436ce0
Silpa Das K D <silpadas.kd@shopalyst.com>
2024-01-03 19:07:57 +0530

    numpy introduction

---
+++ src/python/numpy/three_dim_array.py
@@ -0,0 +1,23 @@
+''' Three Dimensional array..............'''
+
+import numpy as np
+
+arr3 = np.array([
+    [[1,2,3],[4,5,6],[7,8,9]],
+    [[1,1,1],[2,2,2],[3,3,4]],
+    [[4,4,4],[5,5,5],[6,6,6]]
+    ])
+
+print(arr3)
+
+#dimension of array iss
+
+print("dimension of array isss" ,arr3.ndim)
+
+#size of the array is
+
+print("size of the array is ",arr3.size)
+
+#shape of the array
+
+print("shape of array iss ",arr3.shape)  #layer , row , column
---
+++ src/python/numpy/two_dim_array.py
@@ -0,0 +1,15 @@
+''' Two Dimensional array..............'''
+
+import numpy as np
+
+two_array = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+
+# print(two_array)
+
+# print("Dimension of array is ",two_array.ndim)
+
+# create a 2*3 matrix
+
+arr2 = np.array([[1,2,3,4],[4,5,6,6]])
+
+print(arr2.shape) #row,column
